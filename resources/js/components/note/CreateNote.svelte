1a9675ce95b6b0f06a6aff8395c3984b96d4a0d4
Christos Diamantis <xristos567@gmail.com>
2025-11-27 13:39:13 +0200

    refactor(note): simplify CreateNote component by removing unused states and imports
    Removed unnecessary state management and imports from CreateNote.svelte to streamline the component. This enhances readability and maintainability.

--- resources/js/components/note/CreateNote.svelte
+++ resources/js/components/note/CreateNote.svelte
@@ -7,41 +7,12 @@
     import { Textarea } from '@/components/ui/textarea';
     import InputError from '@/components/InputError.svelte';
     import { Button } from '@/components/ui/button';
-    import * as Sheet from "@/components/ui/sheet";
-    import { Settings } from 'lucide-svelte';
-    import Ellipsis from "@lucide/svelte/icons/ellipsis";
-
 
     interface Props {
-        notes: Note[];
         book: Book;
     }
 
-    let { book } = $props();
-
-    let note: Note = $state({
-        ulid: '',
-        body: '',
-        details: '',
-        book_ulid: '',
-        created_at: '',
-        updated_at: '',
-    })
-
-    let fullOpen = $state(false);
-
-    function getFullOpen() {
-        return fullOpen;
-    }
-    
-    function setFullOpen(newOpen: boolean) {
-        fullOpen = newOpen;
-    }
-
-    function toggleFullOpen() {
-        fullOpen = !fullOpen;
-    }
-
+    let { book }: Props = $props();
 
 </script>
 
@@ -54,7 +25,6 @@
             only: ['notes'],
             }}
         resetOnSuccess
-        onSuccess={() => setFullOpen(false)}
         class="space-y-4"
     >
         {#snippet children({
@@ -70,56 +40,15 @@
             <div class="grid gap-2">
                 <Label hidden for="body">Body</Label>
                 <Textarea 
-                    bind:value={note.body}
                     name="body" 
                     class="mt-1 block w-full" 
                     required placeholder="Note body..."
-                    disabled={fullOpen === true}
                 />
                 <InputError class="mt-2" message={errors.body} />
             </div>
-    
             <div class="flex items-center gap-1">
                 <Button type="submit" disabled={processing}>New</Button>
-                <Button variant="ghost" onclick={() => setFullOpen(true)} type="button">
-                    <Ellipsis class="h-4 w-4" />
-                    <span class="sr-only">Open Full</span>
-                </Button>
-            </div>
-            <Sheet.Root bind:open={getFullOpen, setFullOpen}>
-                <Sheet.Content portalProps={{disabled: true}} class="w-full px-2 space-y-4">
-                    <Sheet.Header>
-                        <Sheet.Title class="text-sm font-light">Add a note on <span class="italic">{book.title}</span></Sheet.Title>
-                    </Sheet.Header>
-                    <div class="grid gap-2">
-                        <Label hidden for="body">Body</Label>
-                        <Textarea 
-                            bind:value={note.body}
-                            name="body" 
-                            class="mt-1 block w-full" 
-                            required placeholder="Note body..."
-                            disabled={fullOpen === false}
-                        />
-                        <InputError class="mt-2" message={errors.body} />
-                    </div>
-                    <div class="grid gap-2">
-                        <Label hidden for="details">Details</Label>
-                        <Textarea 
-                            bind:value={note.details}
-                            name="details" 
-                            class="mt-1 block w-full" 
-                            required placeholder="Note details..."
-                            disabled={fullOpen === false}
-                        />
-                        <InputError class="mt-2" message={errors.details} />
-                    </div>
-                    <div class="flex items-center gap-4">
-                        <Button type="submit" disabled={processing}>New</Button>
-                        <Button onclick={() => setFullOpen(false)} type="button">Close Full</Button>
-                    </div>
-                </Sheet.Content>
-            </Sheet.Root>  
-            
+            </div>            
         {/snippet}
     </Form> 
 </div>
--- resources/js/components/note/Note.svelte
+++ resources/js/components/note/Note.svelte
@@ -9,7 +9,7 @@
     import { Label } from '@/components/ui/label';
     import { Textarea } from '@/components/ui/textarea';
     import InputError from '@/components/InputError.svelte';
-    import { Settings, SquarePen } from 'lucide-svelte';
+    import { Eye, Settings, SquarePen } from 'lucide-svelte';
     import BookController from '@/actions/App/Http/Controllers/BookController';
     import NoteController from '@/actions/App/Http/Controllers/NoteController';
     import { inertia, Link } from '@inertiajs/svelte'
@@ -26,11 +26,21 @@
 
 
 <div in:fly={{ duration: 300, easing: sineIn, y: -100, opacity: .5 }} class="mb-4 p-4 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm bg-white dark:bg-gray-800">
-    <p class="mb-2">{note.body}</p>
-    <Button variant="ghost">
-        <Link href={NoteController.show([book.ulid, note.ulid])}>
-            <Settings class="h-4 w-4" />
-            <span class="sr-only">Open Full</span>
-        </Link>
-    </Button>  
+    <div>
+        <div>
+            <p class="mb-2">{note.body}</p>
+        </div>
+        <div class="flex items-center justify-end gap-2">
+
+            <Button variant="ghost">
+                <Link href={NoteController.show([book.ulid, note.ulid])} class="flex items-center">
+                    {#if note.details}
+                        <span class="text-sm text-gray-500 dark:text-gray-400 leading-none pr-2">more lines</span>
+                    {/if}
+                    <Eye class="h-4 w-4 mr-1" />
+                    <span class="sr-only">Open Note</span>
+                </Link>
+            </Button>  
+        </div>
+    </div>
 </div>
